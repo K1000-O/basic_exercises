9ff843243597780bbdd0e069e2150cd7d4b7506a
K1000-O <cjeneconde@gmail.com>
2023-03-03 01:08:35 +0100

    Started the connect 4 game.

---
+++ C/connect4/types.h
@@ -0,0 +1,20 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <strings.h>
+
+typedef enum {
+    ERROR = 0, 
+    OK = 1
+} status;
+
+typedef enum {
+    False = 0, 
+    True = 1
+} Bool;
+
+#endif
